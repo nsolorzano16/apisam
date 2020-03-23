89459f02724b62d49af8a1ac4b931b000570817d
nsolorzano16 <neldavid1993@hotmail.com>
2020-03-23 16:09:48 -0600

    * AntecedentesFamiliaresPersonales.cs: todas las interfaces
    * Startup.cs:
    * Pais.cs:
    * Notas.cs:
    * NotasRepo.cs:
    * Usuario.cs:
    * Habitos.cs:
    * INotas.cs:
    * Religion.cs:
    * HabitosRepo.cs:
    * Paciente.cs:
    * Profesion.cs:
    * Municipio.cs:
    * FarmacosRepo.cs:
    * UsuariosRepo.cs:
    * apisam.web.csproj:
    * Preclinica.cs:
    * PacientesRepo.cs:
    * IHabitos.cs:
    * GrupoEtnico.cs:
    * PreclinicaRepo.cs:
    * Escolaridad.cs:
    * IReligion.cs:
    * Departamento.cs:
    * Diagnosticos.cs:
    * ExamenFisico.cs:
    * apisam.repos.csproj:
    * IPreclinica.cs:
    * AntecedentesRepo.cs:
    * DiagnosticosRepo.cs:
    * ExamenFisicoRepo.cs:
    * GrupoSanguineo.cs:
    * IExamenFisico.cs:
    * IDiagnosticos.cs:
    * FarmacosUsoActual.cs:
    * apisam.entities.csproj:
    * IFarmacosUsoActual.cs:
    * HistorialGinecoObstetra.cs:
    * HistorialGinecoObstetraRepo.cs:
    * ExamenFisicoGinecologico.cs:
    * ConsultaController.cs:
    * UsuariosController.cs:
    * ExamenFisicoGinecologicoRepo.cs:
    * IHistorialGinecoObstetra.cs:
    * PacientesController.cs:
    * PreclinicaController.cs:
    * IExamenFisicoGinecologico.cs:
    * ConsultaViewModel.cs:
    * PreclinicaViewModel.cs:
    * IAntecedentesFamiliaresPersonales.cs:

---
+++ apisam.interfaces/IReligion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using apisam.entities;
+
+namespace apisam.interfaces
+{
+    public interface IReligion
+    {
+        List<Religion> Religiones { get; }
+        bool Add(Religion religion);
+        bool Update(Religion religion);
+        Religion GetReligionById(int id);
+    }
+}
