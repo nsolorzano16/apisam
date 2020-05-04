cfcc6c41d13869e86472c1313f10a310b311ee50
nsolorzano16 <neldavid1993@hotmail.com>
2020-05-04 17:58:02 -0600

    file

---
+++ apisam.entities/ExamenIndicado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ServiceStack.DataAnnotations;
+
+namespace apisam.entities
+{
+    public class ExamenIndicado : RegistroBase
+    {
+        public ExamenIndicado()
+        {
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int ExamenIndicadoId { get; set; }
+        public int PacienteId { get; set; }
+        public int DoctorId { get; set; }
+        public int PreclinicaId { get; set; }
+        public int ExamenCategoriaId { get; set; }
+        public int ExamenTipoId { get; set; }
+        public int ExamenDetalleId { get; set; }
+        public string Nombre { get; set; }
+
+
+
+    }
+}
