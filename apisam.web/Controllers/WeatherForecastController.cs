43d6590da93be701a2ae7e6c6a1e43f8932a5859
Nelson Solorzano <neldavid1993@hotmail.com>
2020-07-02 11:59:18 -0600

    second commit

--- apisam.web/Controllers/WeatherForecastController.cs
+++ apisam.web/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace apisam.web.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warmf", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
